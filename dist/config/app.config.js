710058a797a25d49a6657f5008f691d299caea8e
Jashwanth S Poojary <149219296+JashwanthSPoojary@users.noreply.github.com>
2025-05-23 20:55:48 +0530

    Delete dist directory

--- dist/config/app.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appConfig = void 0;
-exports.appConfig = {
-    port: parseInt(process.env.PORT || '3000'),
-    env: process.env.NODE_ENV || 'development',
-    apiPrefix: '/api/v1',
-};
--- dist/shared/middleware/not-found.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = notFoundHandler;
-const response_utils_1 = require("../utils/response.utils");
-function notFoundHandler(req, res, next) {
-    (0, response_utils_1.sendErrorResponse)(res, 'Endpoint not found', 404, 'NOT_FOUND');
-}
